a2aeb78b9fa3ecdfd4fea8d1f4ef3948193fe9a3
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-11 01:43:37 +0700

    add TrackReply model

---
+++ Models/TrackReply.cs
@@ -0,0 +1,9 @@
+﻿namespace sweep_email_api.Models
+{
+    public class TrackReply
+    {
+        public string TrackId { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+    }
+}
