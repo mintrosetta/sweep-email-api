fa1f445feff54910b09778fa5e13a7b6246c024c
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-11 10:46:09 +0700

    add ResponseDto

---
+++ Dto/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace sweep_email_api.Dto
+{
+    public class ResponseDto<T>
+    {
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+    }
+}
