99994327ea1be401078a0fc0af4c0829164535e9
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-11 12:48:11 +0700

    add RecurringJob for fetch new reply

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using Hangfire;
+using Hangfire.Storage.SQLite;
 using sweep_email_api.Data.Configurations;
 using sweep_email_api.Services;
+using sweep_email_api.Services.HangfireJobs;
 using sweep_email_api.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,14 @@ builder.Services.Configure<ImapGmail>(builder.Configuration.GetSection("MailSett
 
 // Add services to the container.
 builder.Services.AddScoped<ISweepService, SweepService>();
+builder.Services.AddScoped<SweepRepliesJob>();
+
+// initial hangfire configuration
+builder.Services.AddHangfire(config => config.UseSimpleAssemblyNameTypeSerializer()
+                                             .UseRecommendedSerializerSettings()
+                                             .UseSQLiteStorage($"Data Source=hangfire.db;"));
+builder.Services.AddHangfireServer();
+
 
 // tell .NET use Controller
 builder.Services.AddControllers();
@@ -34,5 +45,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// configuration hangfire dashboard
+app.UseHangfireDashboard();
+app.MapHangfireDashboard();
+
+RecurringJob.AddOrUpdate<SweepRepliesJob>("SweepJob", x => x.Run(), Cron.Minutely());
 
 app.Run();
---
+++ Services/HangfireJobs/SweepRepliesJob.cs
@@ -0,0 +1,21 @@
+﻿using sweep_email_api.Services.Interfaces;
+
+namespace sweep_email_api.Services.HangfireJobs
+{
+    public class SweepRepliesJob
+    {
+        private readonly ISweepService sweepService;
+
+        public SweepRepliesJob(ISweepService sweepService)
+        {
+            this.sweepService = sweepService;
+            Console.WriteLine("Start job");
+        }
+
+        public async Task Run()
+        {
+            var result = await this.sweepService.FetchRepliesAsync();
+            Console.WriteLine($"Count replies is: {result.Count()}");
+        }
+    }
+}
